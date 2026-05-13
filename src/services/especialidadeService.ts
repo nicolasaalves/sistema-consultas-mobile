4142f78a32bb3b10beacd64c9131fac6c6f1932c
nicolasaalves <nicolasantos2410@gmail.com>
2026-05-12 22:32:32 -0300

    "feat - Integração Frontend e Backend"

---
+++ src/services/especialidadeService.ts
@@ -0,0 +1,17 @@
+import api from "./api";
+import { Especialidade } from "../types/especialidade";
+
+export async function listarEspecialidades(): Promise<Especialidade[]> {
+ const response = await api.get<Especialidade[]>("/especialidades");
+ return response.data;
+}
+
+export async function buscarEspecialidadePorId(id: number): Promise<Especialidade> {
+ const response = await api.get<Especialidade>(`/especialidades/${id}`);
+ return response.data;
+}
+
+export async function criarEspecialidade(especialidade: Omit<Especialidade, "id">): Promise<Especialidade> {
+ const response = await api.post<Especialidade>("/especialidades", especialidade);
+ return response.data;
+}
